03af3cd465b43eac8ae565cdf5125b0e772a5c20
Nicolas Dumont <nicolas.dumont.perso@gmail.com>
2014-11-20 22:04:05 +0100

    Compilation OK + try PROGMEM

--- domotix.ino
+++ domotix.ino
@@ -1,13 +1,31 @@
-#include <SD.h>
 #include <SPI.h>
+#include <Time.h>
+#include <SD.h>
 #include <Ethernet.h>
+#include <LiquidCrystal.h>
 
-#define DEBUG
+/* #define DEBUG */
 
 /********************************************************/
 /*      Pin  definitions                                */
 /********************************************************/
+#define PIN_A0		A0
+#define PIN_A1		A1
+#define PIN_A2		A2
+#define PIN_A3		A3
+#define PIN_A4		A4
+#define PIN_A5		A5
+
+#define PIN_9		9
+#define PIN_8		8
+#define PIN_7		7
+#define PIN_6		6
+#define PIN_5		5
 #define CS_PIN_SDCARD   4
+#define PIN_3		3
+#define PIN_2		2
+
+
 
 /********************************************************/
 /*      Process definitions                             */
@@ -25,6 +43,10 @@ uint8_t g_process_domotix;
 #define PROCESS_DOMOTIX_OFF		0
 #define PROCESS_DOMOTIX_ON		1
 
+uint8_t g_process_time;
+#define PROCESS_TIME_OFF		0
+#define PROCESS_TIME_ON			1
+
 /********************************************************/
 /*      Global definitions                              */
 /********************************************************/
@@ -39,7 +61,6 @@ char g_line[LINE_MAX_LEN+1];
 uint8_t g_buff[BUFF_MAX_SIZE];
 
 char g_root_filename[] = "index.htm";
-
 
 typedef struct code_t
 {
@@ -71,12 +92,46 @@ EthernetClient g_client;
 uint8_t g_page_web   = 0;
 uint16_t g_req_count = 0;
 File g_file_html;
+time_t g_prevDisplay = 0;
+char g_bufferTxt[80];
+
+/********************************************************/
+/*      NTP			                        */
+/********************************************************/
+
+#define TIMEZONE			2
+#define LOCAL_PORT_NTP			8888
+#define NTP_PACKET_SIZE			48
+#define TIME_SYNCHRO_SEC		200
+
+/* server Free */
+IPAddress g_timeServer(132, 163, 4, 101);
+EthernetUDP g_Udp;
+uint32_t g_beginWait;
+time_t prevDisplay = 0;
+
+/*  NTP time is in the first 48 bytes of message*/
+byte g_packetBuffer[NTP_PACKET_SIZE];
+
+
+/********************************************************/
+/*      CONSTANT STRING		                        */
+/********************************************************/
+const char g_txtError404[] PROGMEM = "HTTP/1.1 404 Not Found";
+
+
 
 /********************************************************/
 /*      Init			                        */
 /********************************************************/
 void setup(void)
 {
+    /* init Process */
+    g_process_serial   = PROCESS_SERIAL_ON;
+    g_process_ethernet = PROCESS_ETHERNET_ON;
+    g_process_domotix  = PROCESS_DOMOTIX_OFF;
+    g_process_time     = PROCESS_TIME_OFF;
+
     /* initialize serial communications at 115200 bps */
     Serial.begin(115200);
 
@@ -89,12 +144,17 @@ void setup(void)
     SD.begin(CS_PIN_SDCARD);
     delay(100);
 
+    /* init UDP for NTP */
+    g_Udp.begin(LOCAL_PORT_NTP);
+    setSyncInterval(TIME_SYNCHRO_SEC);
+    setSyncProvider(getNtpTime);
+    delay(100);
+
+    /* Init Port In/Out */
+    /*pinMode(PIN_LED, OUTPUT);*/
+
     /* Init global var */
 
-    /* init Process */
-    g_process_serial   = PROCESS_SERIAL_ON;
-    g_process_ethernet = PROCESS_ETHERNET_ON;
-    g_process_domotix  = PROCESS_DOMOTIX_OFF;
 
 #ifdef DEBUG
     Serial.print("Init OK\r\n");
@@ -309,6 +369,10 @@ void deal_with_code(char type, uint8_t code)
 	    else
 		g_client.write((const uint8_t*)ptr_code->name0, ptr_code->len0);
 	}break;
+	case 'W':
+	{
+	    digitalClockDisplay();
+	}
     }
 }
 
@@ -375,6 +439,154 @@ void send_resp_to_client(File *file)
     file->close();
 }
 
+/********************************************************/
+/*      NTP functions                                   */
+/********************************************************/
+void digitalClockDisplaySerial(void)
+{
+    Serial.print(hour());
+    Serial.print (":");
+    printDigitsSerial(minute());
+    Serial.print (":");
+    printDigitsSerial(second());
+    Serial.print(" ");
+    Serial.print(day());
+    Serial.print("/");
+    Serial.print(month());
+    Serial.print("/");
+    Serial.print(year());
+    Serial.println();
+}
+
+void printDigitsSerial(int digits)
+{
+    if (digits < 10)
+	Serial.print('0');
+    Serial.print(digits);
+}
+
+void digitalClockDisplay(void)
+{
+    g_client.print(hour());
+    g_client.print (":");
+    printDigits(minute());
+    g_client.print (":");
+    printDigits(second());
+    g_client.print(" ");
+    g_client.print(day());
+    g_client.print("/");
+    g_client.print(month());
+    g_client.print("/");
+    g_client.print(year());
+    g_client.println();
+}
+
+void printDigits(int digits)
+{
+    if (digits < 10)
+	g_client.print('0');
+    g_client.print(digits);
+}
+
+
+time_t getNtpTime(void)
+{
+    int size;
+    unsigned long secsSince1900;
+
+    /* discard any previously received packets */
+    while (g_Udp.parsePacket() > 0);
+
+#ifdef DEBUG
+    Serial.println("Transmit NTP");
+#endif
+
+    sendNTPpacket(g_timeServer);
+
+    g_beginWait = millis();
+    while (millis() - g_beginWait < 1500)
+    {
+	size = g_Udp.parsePacket();
+
+	if (size >= NTP_PACKET_SIZE)
+	{
+
+#ifdef DEBUG
+	    Serial.println("Receive NTP");
+#endif
+
+	    g_Udp.read(g_packetBuffer, NTP_PACKET_SIZE);
+
+	    /* convert four bytes starting at location 40 to a long integer */
+	    secsSince1900 =  (unsigned long)g_packetBuffer[40] << 24;
+	    secsSince1900 |= (unsigned long)g_packetBuffer[41] << 16;
+	    secsSince1900 |= (unsigned long)g_packetBuffer[42] << 8;
+	    secsSince1900 |= (unsigned long)g_packetBuffer[43];
+	    return secsSince1900 - 2208988800UL + TIMEZONE * SECS_PER_HOUR;
+	}
+    }
+    Serial.println("No NTP :-(");
+    return 0;
+}
+
+/* send an NTP request to the time server at the given address */
+void sendNTPpacket(IPAddress &address)
+{
+    /* set all bytes in the buffer to 0 */
+    memset(g_packetBuffer, 0, NTP_PACKET_SIZE);
+
+    /* Initialize values needed to form NTP request */
+    /* (see URL above for details on the packets) */
+    g_packetBuffer[0] = 0b11100011;   /* LI, Version, Mode */
+    g_packetBuffer[1] = 0;     /* Stratum, or type of clock */
+    g_packetBuffer[2] = 6;     /* Polling Interval */
+    g_packetBuffer[3] = 0xEC;  /* Peer Clock Precision */
+
+    /* 8 bytes of zero for Root Delay & Root Dispersion */
+    g_packetBuffer[12]  = 49;
+    g_packetBuffer[13]  = 0x4E;
+    g_packetBuffer[14]  = 49;
+    g_packetBuffer[15]  = 52;
+
+    /* all NTP fields have been given values, now
+     * you can send a packet requesting a timestamp:
+     * NTP requests are to port 123
+     */
+    g_Udp.beginPacket(address, 123);
+    g_Udp.write(g_packetBuffer, NTP_PACKET_SIZE);
+    g_Udp.endPacket();
+}
+
+void printwebln(char *text)
+{
+    strcpy_P(buffer,text);
+   strcpy_P(buffer, (PGM_P)pgm_read_word(&(dayNames_P[day])));
+
+    if (ln == 1)
+    {
+	g_client.println(buffer);
+    }
+    else
+    {
+	g_client.print(buffer);
+    }
+}
+
+void printserial(char *text, uint8_t ln)
+{
+    char buffer[80];
+    strcpy_P(buffer,text);
+
+    if (ln == 1)
+    {
+	g_client.println(buffer);
+    }
+    else
+    {
+	g_client.print(buffer);
+    }
+}
+
 
 /********************************************************/
 /*      Process                                         */
@@ -397,10 +609,7 @@ void process_serial(void)
 	    {
 		case 'a':
 		{
-
-#ifdef DEBUG
-			    Serial.print("Bonjour\r\n");
-#endif
+			    digitalClockDisplaySerial();
 		}break;
 	    }
 
@@ -501,12 +710,12 @@ void process_ethernet(void)
 				else if (strstr(filename, ".jpg") != 0)
 				{
 				    g_client.println("Content-Type: image/jpeg");
-				    g_client.println("Cache-Control: max-age=2592000");
+				    /* g_client.println("Cache-Control: max-age=2592000"); */
 				}
 				else if (strstr(filename, ".ico") != 0)
 				{
 				    g_client.println("Content-Type: image/x-icon");
-				    g_client.println("Cache-Control: max-age=2592000");
+				    /* g_client.println("Cache-Control: max-age=2592000"); */
 				}
 				else
 				    g_client.println("Content-Type: text");
@@ -521,9 +730,9 @@ void process_ethernet(void)
 			    else
 			    {
 				g_client.println("HTTP/1.1 404 Not Found");
-				g_client.println("Content-Type: text/html");
+				/* g_client.println("Content-Type: text/html"); */
 				g_client.println();
-				g_client.println("<h2>Domotix Error: File Not Found!</h2>");
+				/* g_client.println("<h2>Domotix Error: File Not Found!</h2>"); */
 
 				break;
 			    }
@@ -531,9 +740,9 @@ void process_ethernet(void)
 			else
 			{
 			    g_client.println("HTTP/1.1 404 Not Found");
-			    g_client.println("Content-Type: text/html");
+			    /* g_client.println("Content-Type: text/html"); */
 			    g_client.println();
-			    g_client.println("<h2>Domotix Error: GET /  Not Found!</h2>");
+			    /* g_client.println("<h2>Domotix Error: GET /  Not Found!</h2>"); */
 
 			    break;
 			}
@@ -558,9 +767,31 @@ void process_domotix(void)
     }
 }
 
+
+void process_time(void)
+{
+    time_t  curr_time;
+
+    if (g_process_time != PROCESS_TIME_OFF)
+    {
+	if (timeStatus() != timeNotSet)
+	{
+	    curr_time = now();
+	    if (curr_time != g_prevDisplay)
+	    {
+		g_prevDisplay = curr_time;
+		digitalClockDisplay();
+	    }
+	}
+    }
+}
+
+
+
 void loop(void)
 {
     process_ethernet();
     process_serial();
+    process_time();
     process_domotix();
 }
