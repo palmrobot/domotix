ebc2259044be3d6aca716be67719be6f4529ff70
Nicolas Dumont <nicolas.dumont.perso@gmail.com>
2015-05-07 18:20:56 +0200

    start fix bug on sdcard

--- domotix.ino
+++ domotix.ino
@@ -10,7 +10,7 @@
 /* #define DEBUG_SENSOR */
 /* #define DEBUG_ITEM */
 
-#define VERSION				"v2.7"
+#define VERSION				"v2.8"
 
 /********************************************************/
 /*      Pin  definitions                                */
