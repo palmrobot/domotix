b0ef9404eb3ef05c619c50a2512bcff064522b00
Nicolas Dumont <nicolas.dumont.perso@gmail.com>
2016-01-03 21:37:03 +0100

    Add detection off freeze on the door or too long openning door, add hysteresys on presence poule sensors

--- domotix_poulailler/domotix_poulailler.ino
+++ domotix_poulailler/domotix_poulailler.ino
@@ -28,20 +28,17 @@
 #define STATE_CLOSED			3
 #define STATE_UNKNOWN			4
 
-#define TIME_TO_CLOSE_THE_DOOR		1800
-#define TIME_TO_OPEN_THE_DOOR		700
-#define TIME_TO_RESET_COUNTERS		0
-
-#define THRESHOLD_POULE			400
+#define THRESHOLD_POULE			300
 
 #define DOOR_CLOSED			1
 #define DOOR_OPENED			0
 
-#define PONDOIR_POULE			0
-#define PONDOIR_EMPTY			1
+#define PONDOIR_POULE			1
+#define PONDOIR_EMPTY			0
 
 uint8_t g_process_serial;
-uint8_t g_process_action;
+uint8_t g_process_door;
+uint8_t g_process_poule;
 
 uint8_t g_process_delay;
 #define PROCESS_DELAY_OFF			0
@@ -62,13 +59,26 @@ typedef struct
 #define NB_DELAY_MAX			10
 delay_t g_delay[NB_DELAY_MAX];
 
+time_t g_current_time;
+time_t g_openning_time;
+time_t g_closing_time;
+
+time_t g_current_poule_gauche_time;
+time_t g_previous_poule_gauche_time;
+uint8_t g_poule_gauche_state;
+uint8_t g_poule_gauche_previous_state;
+
+time_t g_current_poule_droite_time;
+time_t g_previous_poule_droite_time;
+uint8_t g_poule_droite_state;
+uint8_t g_poule_droite_previous_state;
+
 void open_the_door(void)
 {
     /* open the door */
     digitalWrite(PIN_OUT_MOT_DIR_1, LOW);
     digitalWrite(PIN_OUT_MOT_DIR_2, HIGH);
     digitalWrite(PIN_OUT_MOT_SPEED, HIGH);
-    g_state_current = STATE_OPENNING;
 }
 
 void close_the_door(void)
@@ -77,14 +87,19 @@ void close_the_door(void)
     digitalWrite(PIN_OUT_MOT_DIR_1, HIGH);
     digitalWrite(PIN_OUT_MOT_DIR_2, LOW);
     digitalWrite(PIN_OUT_MOT_SPEED, HIGH);
-    g_state_current = STATE_CLOSING;
+}
+
+void stop_motor(void)
+{
+    digitalWrite(PIN_OUT_MOT_DIR_1, LOW);
+    digitalWrite(PIN_OUT_MOT_DIR_2, LOW);
+    digitalWrite(PIN_OUT_MOT_SPEED, HIGH);
+    delay(500);
+    digitalWrite(PIN_OUT_MOT_SPEED, LOW);
 }
 
 void setup()
 {
-    uint8_t opened;
-    uint8_t closed;
-
     /* Init input/output */
     pinMode(PIN_OUT_MOT_DIR_1, OUTPUT);
     pinMode(PIN_OUT_MOT_DIR_2, OUTPUT);
@@ -108,42 +123,38 @@ void setup()
 
     /* Init global variables */
     g_process_serial	= 0;
-    g_process_action    = 1;
+    g_process_door      = 1;
+    g_process_poule     = 1;
 
-    opened  = digitalRead(PIN_IN_SENSOR_OPENED);
-    closed = digitalRead(PIN_IN_SENSOR_CLOSED);
-    if (opened == 1)
-    {
-	g_state_current  = STATE_OPENED;
-    }
-    else if (closed == 1)
-    {
-	g_state_current  = STATE_CLOSED;
-    }
-    else
-    {
-	g_state_current  = STATE_OPENNING;
-    }
+    /* Set arbitrary state */
+    g_state_current  = STATE_OPENNING;
 
     /* initialize serial communications at 115200 bps: */
     Serial.begin(115200);
     delay(100);
+
+    setTime(0,0,0,20,11,15);
+    g_current_time  = now();
+    g_closing_time  = g_current_time;
+    g_openning_time = g_current_time;
+    g_previous_poule_gauche_time = g_current_time;
+    g_previous_poule_droite_time = g_current_time;
 }
 
 
-void process_action(void)
+void process_door(void)
 {
     uint8_t move_the_door;
     uint8_t opened;
     uint8_t closed;
-    uint8_t poule_gauche;
-    uint8_t poule_droite;
 
-    if (g_process_action != 0)
+    if (g_process_door != 0)
     {
-	opened  = digitalRead(PIN_IN_SENSOR_OPENED);
+	/* First, Check Current state */
+	opened = digitalRead(PIN_IN_SENSOR_OPENED);
 	closed = digitalRead(PIN_IN_SENSOR_CLOSED);
 
+	/* Door is closed, check if it has just happened */
 	if (closed == 1)
 	{
 	    if (g_state_current == STATE_CLOSING)
@@ -151,15 +162,29 @@ void process_action(void)
 		g_state_current  = STATE_CLOSED;
 
 		/* Stop immediatly */
-		digitalWrite(PIN_OUT_MOT_DIR_1, LOW);
-		digitalWrite(PIN_OUT_MOT_DIR_2, LOW);
-		digitalWrite(PIN_OUT_MOT_SPEED, HIGH);
-		delay(1000);
-		digitalWrite(PIN_OUT_MOT_SPEED, LOW);
+		stop_motor();
 
+		/* Then send info to Domotix */
 		digitalWrite(PIN_OUT_DOMOTIX_STATE_DOOR, DOOR_CLOSED);
 	    }
+	    else if (g_state_current == STATE_OPENNING)
+	    {
+		/* check if door is blocked */
+		g_current_time = now();
+		if (g_current_time > (g_openning_time + 4))
+		{
+		    /* something when wrong, door is blocked */
+		    g_state_current  = STATE_CLOSED;
+
+		    /* Stop immediatly */
+		    stop_motor();
+
+		    /* Then send info to Domotix */
+		    digitalWrite(PIN_OUT_DOMOTIX_STATE_DOOR, DOOR_CLOSED);
+		}
+	    }
 	}
+	/* Door is openned, check if it has just happened */
 	else if (opened == 1)
 	{
 	    if (g_state_current == STATE_OPENNING)
@@ -167,57 +192,169 @@ void process_action(void)
 		g_state_current  = STATE_OPENED;
 
 		/* Stop immediatly */
-		digitalWrite(PIN_OUT_MOT_DIR_1, LOW);
-		digitalWrite(PIN_OUT_MOT_DIR_2, LOW);
-		digitalWrite(PIN_OUT_MOT_SPEED, HIGH);
-		delay(1000);
-		digitalWrite(PIN_OUT_MOT_SPEED, LOW);
+		stop_motor();
 
+		/* Then send info to Domotix */
 		digitalWrite(PIN_OUT_DOMOTIX_STATE_DOOR, DOOR_OPENED);
+	    }
+	    else if (g_state_current == STATE_CLOSING)
+	    {
+		/* check if door is blocked */
+		g_current_time = now();
+		if (g_current_time > (g_closing_time + 4))
+		{
+		    /* something when wrong, door is blocked */
+		    g_state_current  = STATE_OPENED;
+
+		    /* Stop immediatly */
+		    stop_motor();
+
+		    /* Then send info to Domotix */
+		    digitalWrite(PIN_OUT_DOMOTIX_STATE_DOOR, DOOR_OPENED);
+		}
 	    }
 	}
 
 	/* Check if outdoor button is pressed */
 	move_the_door  = digitalRead(PIN_IN_SENSOR_ACTION_MOVE);
-	if (move_the_door == 0)
+	if (move_the_door == 1)
+	{
+	    /* Wait for button released */
+	    while (digitalRead(PIN_IN_SENSOR_ACTION_MOVE) == 1)
+	    {
+		delay(10);
+	    }
+	}
+	else
 	{
 	    /* Check if door must be openned or closed by action domotix */
 	    move_the_door = digitalRead(PIN_IN_DOMOTIX_ACTION_MOVE);
+	    if (move_the_door == 1)
+	    {
+		while (digitalRead(PIN_IN_DOMOTIX_ACTION_MOVE) == 1)
+		{
+		    delay(10);
+		}
+	    }
 	}
 
 	if ((move_the_door) && ((g_state_current == STATE_CLOSED) || (g_state_current == STATE_CLOSING)))
 	{
 	    open_the_door();
-
-	    /* wait some time before reading sensor */
-	    delay(2000);
+	    g_state_current = STATE_OPENNING;
+	    g_openning_time = now();
 	}
 	else if ((move_the_door) && ((g_state_current == STATE_OPENED) || (g_state_current == STATE_OPENNING)))
 	{
 	    close_the_door();
-
-	    /* wait some time before reading sensor */
-	    delay(2000);
+	    g_state_current = STATE_CLOSING;
+	    g_closing_time = now();
 	}
 
+
+	/* check if it's not openning or closing for a too long time !!! */
+	if (g_state_current == STATE_OPENNING)
+	{
+	    g_current_time = now();
+	    if (g_current_time > (g_openning_time + 60))
+	    {
+		/* Too long to open the door, then stop motor */
+		/* Stop immediatly */
+		g_state_current  = STATE_OPENED;
+
+		/* Stop immediatly */
+		stop_motor();
+
+		/* Then send info to Domotix */
+		digitalWrite(PIN_OUT_DOMOTIX_STATE_DOOR, DOOR_OPENED);
+	    }
+	}
+	else if (g_state_current == STATE_CLOSING)
+	{
+	    g_current_time = now();
+	    if (g_current_time > (g_closing_time + 60))
+	    {
+		/* Too long to close the door, then stop motor */
+		g_state_current  = STATE_CLOSED;
+
+		/* Stop immediatly */
+		stop_motor();
+
+		/* Then send info to Domotix */
+		digitalWrite(PIN_OUT_DOMOTIX_STATE_DOOR, DOOR_CLOSED);
+	    }
+	}
+    }
+}
+
+void process_poule(void)
+{
+    uint16_t poule_gauche;
+    uint16_t poule_droite;
+
+    if (g_process_poule != 0)
+    {
+	/* convert analog read to binary state */
 	poule_gauche = analogRead(PIN_IN_SENSOR_PONDOIR_GAUCHE);
 	if (poule_gauche > THRESHOLD_POULE)
 	{
-	    digitalWrite(PIN_OUT_DOMOTIX_POULE_GAUCHE, PONDOIR_POULE);
+	    g_poule_gauche_state = PONDOIR_POULE;
 	}
 	else
 	{
-	    digitalWrite(PIN_OUT_DOMOTIX_POULE_GAUCHE, PONDOIR_EMPTY);
+	    g_poule_gauche_state = PONDOIR_EMPTY;
 	}
+
+	/* check if it's really empty or full
+	 * wait some time and check again
+	 */
+	if (g_poule_gauche_previous_state != g_poule_gauche_state)
+	{
+	    g_poule_gauche_previous_state = g_poule_gauche_state;
+
+	    /* start counting time */
+	    g_previous_poule_gauche_time = now();
+	}
+	else
+	{
+	    g_current_poule_gauche_time = now();
+	    if (g_current_poule_gauche_time > (g_previous_poule_gauche_time + 240))
+	    {
+		digitalWrite(PIN_OUT_DOMOTIX_POULE_GAUCHE, g_poule_gauche_state);
+		g_previous_poule_gauche_time = g_poule_gauche_state;
+	    }
+	}
+
+
 
 	poule_droite = analogRead(PIN_IN_SENSOR_PONDOIR_DROITE);
 	if (poule_droite > THRESHOLD_POULE)
 	{
-	    digitalWrite(PIN_OUT_DOMOTIX_POULE_DROITE, PONDOIR_POULE);
+	    g_poule_droite_state = PONDOIR_POULE;
 	}
 	else
 	{
-	    digitalWrite(PIN_OUT_DOMOTIX_POULE_DROITE, PONDOIR_EMPTY);
+	    g_poule_droite_state = PONDOIR_EMPTY;
+	}
+
+	/* check if it's really empty or full
+	 * wait some time and check again
+	 */
+	if (g_poule_droite_previous_state != g_poule_droite_state)
+	{
+	    g_poule_droite_previous_state = g_poule_droite_state;
+
+	    /* start counting time */
+	    g_previous_poule_droite_time = now();
+	}
+	else
+	{
+	    g_current_poule_droite_time = now();
+	    if (g_current_poule_droite_time > (g_previous_poule_droite_time + 240))
+	    {
+		digitalWrite(PIN_OUT_DOMOTIX_POULE_GAUCHE, g_poule_droite_state);
+		g_previous_poule_droite_time = g_poule_droite_state;
+	    }
 	}
     }
 }
@@ -278,6 +415,7 @@ void process_delay(void)
 void process_serial(void)
 {
     char value_read;
+    uint8_t sensorValue = 0;
 
     if (g_process_serial)
     {
@@ -320,6 +458,19 @@ void process_serial(void)
 		    digitalWrite(PIN_OUT_MOT_SPEED, LOW);
 		}break;
 
+		case 'f':
+		{
+		    sensorValue = analogRead(PIN_IN_SENSOR_PONDOIR_GAUCHE);
+
+		    Serial.print("pondoir gauche = ");
+		    Serial.print(sensorValue);
+		    sensorValue = analogRead(PIN_IN_SENSOR_PONDOIR_DROITE);
+
+		    Serial.print(" --- ");
+		    Serial.println(sensorValue);
+
+		}break;
+
 		default:
 
 		break;
@@ -331,7 +482,8 @@ void process_serial(void)
 void loop()
 {
     process_serial();
-    process_action();
+    process_door();
+    process_poule();
 }
 
 
